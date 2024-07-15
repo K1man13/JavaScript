501f4158ea2925e550970140c0d71fb52033d139
K1man13 <kym.poetL7@gmail.com>
2024-07-15 12:46:53 +0300

    new

---
+++ nestedloop.js
@@ -0,0 +1,45 @@
+for (let i = 0; i < 3; i++){
+    for (let j = 0; j < 3; j++) {
+       if (j === 1)
+        {
+        continue; // skip 
+    }
+        console.log(`i = ${i}, j = ${j}`)
+    }
+}
+
+
+
+
+// for (let i = 0; i < 10; i++)
+//     {
+//     if (i === 5){
+//         break;
+//     }
+//         console.log("loop with break :" + i);
+// }
+
+
+// for (let i=0; i < 10; i++){
+//     if (i === 8){
+//         continue;
+//     }
+//     console.log( "loop with continue:" + i 
+//     );
+// }
+
+// for (let i=0; i < 10; i++){
+//     if (i%2 === 0){
+//         continue;
+//     }
+//     console.log( "loop with continue:" + i 
+//     );
+// }
+
+// for (let i=0; i < 10; i++){
+//     if (i%2 === 1){
+//         continue;
+//     }
+//     console.log( "loop with continue:" + i 
+//     );
+// }

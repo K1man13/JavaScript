47189b7dc1b8d02deccff86e2d8fd7ba0d197dde
K1man13 <kym.poetL7@gmail.com>
2024-07-15 10:36:52 +0300

    new files

--- loop.js
+++ loop.js
@@ -1,5 +1,9 @@
-for (let number= 0; number<= 20; number+=2) {
-    console.log(number);;
+for (let number= 0; number <= 20; number+=2) {
+     console.log(number);;
     
 }
-
+let i = 0;
+while (i < 5) {
+    console.log(+i);
+    i++;   
+}
---
+++ while.js
@@ -0,0 +1,6 @@
+let i = 0;
+do{
+    console.log("do...while loop iteration...:" + i);
+    i = i + 1;
+} while(i < 5);
+
